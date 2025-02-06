994fa2bc14db1627c8c5be79e90ef2da6c9d41da
Sainan <sainan@calamity.inc>
2025-02-06 14:25:55 +0100

    Update SNS repo link

--- SpaceNinjaServer.git/index.php
+++ SpaceNinjaServer.git/index.php
@@ -1,3 +1,4 @@
 <?php
 //header("Location: https://github.com/spaceninjaserver/SpaceNinjaServer".substr($_SERVER["REQUEST_URI"], 21)); // "/SpaceNinjaServer.git"
-header("Location: http://209.141.38.3/OpenWF/SpaceNinjaServer".substr($_SERVER["REQUEST_URI"], 21)); // "/SpaceNinjaServer.git"
+//header("Location: http://209.141.38.3/OpenWF/SpaceNinjaServer".substr($_SERVER["REQUEST_URI"], 21)); // "/SpaceNinjaServer.git"
+header("Location: https://onlyg.it/OpenWF/SpaceNinjaServer".substr($_SERVER["REQUEST_URI"], 21)); // "/SpaceNinjaServer.git"
