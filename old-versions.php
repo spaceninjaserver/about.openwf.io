16071ecacce6c4510a9d4de303728367e7e77e13
Sainan <63328889+Sainan@users.noreply.github.com>
2025-05-03 07:13:14 +0200

    Update old versions for bootstrapper 0.10.11

--- old-versions.php
+++ old-versions.php
@@ -10,104 +10,247 @@
 	<div class="container mt-3">
 		<h3>Recent Versions</h3>
 		<p>While the Bootstrapper and SpaceNinjaServer do stay up-to-date, they are still compatible with previous versions and should at least allow you to login and play:</p>
-		<table class="table">
+		<table class="table table-sm">
 			<thead>
 				<tr>
-					<th>Code Version</th>
-					<th>Title</th>
 					<th>Steam Manifest ID</th>
+					<th>Version</th>
+					<th>Title</th>
+					<th>Notes</th>
 				</tr>
 			</thead>
 			<tbody>
 				<!-- 4212893560910494140 (38.5.0?) -->
 				<tr>
+					<td>3864063200081954630</td>
 					<td><code>2024.12.11.14.26</code></td>
 					<td>Early build of Warframe: 1999</td>
-					<td>3864063200081954630</td>
+					<td></td>
 				</tr>
 				<tr>
+					<td>513490184226321110</td>
 					<td><code>2024.10.11.10.06</code></td>
 					<td>Koumei & the Five Fates: Hotfix 37.0.6</td>
-					<td>513490184226321110</td>
+					<td></td>
 				</tr>
 				<tr>
+					<td>2655930801276651913</td>
 					<td><code>2024.09.30.16.56</code></td>
 					<td>Early build of Koumei & the Five Fates</td>
-					<td>2655930801276651913</td>
+					<td></td>
 				</tr>
 				<tr>
+					<td>8111233839173446959</td>
 					<td><code>2024.08.21.20.02</code></td>
 					<td>The Lotus Eaters: Hotfix 36.1.2</td>
-					<td>8111233839173446959</td>
+					<td></td>
 				</tr>
 				<tr>
+					<td>7104716125379362906</td>
 					<td><code>2024.07.17.17.07</code></td>
 					<td>Jade Shadows: Hotfix 36.0.7</td>
-					<td>7104716125379362906</td>
+					<td></td>
 				</tr>
 				<tr>
+					<td>8456987056335242580</td>
 					<td><code>2024.06.25.16.44</code></td>
 					<td>Jade Shadows: Hotfix 36.0.4</td>
-					<td>8456987056335242580</td>
+					<td></td>
 				</tr>
 				<tr>
+					<td>8444137865636064549</td>
 					<td><code>2024.06.12.18.42</code></td>
 					<td>Early build of Jade Shadows</td>
-					<td>8444137865636064549</td>
+					<td></td>
 				</tr>
 				<tr>
+					<td>4262681179882731333</td>
 					<td><code>2024.03.24.20.00</code></td>
 					<td>Update 35.5: Dante Unbound</td>
-					<td>4262681179882731333</td>
+					<td></td>
 				</tr>
 				<tr>
+					<td>3417254152679980994</td>
 					<td><code>2024.02.16.17.13</code></td>
 					<td>Whispers in the Walls: Update 35.1.0</td>
-					<td>3417254152679980994</td>
+					<td></td>
 				</tr>
 				<!-- Also verified that 34.0.8 works, but sadly there's no manifest id for any of U34.-->
 				<!-- Beyond this point, there is no information to help correlate the build labels, so I'm just writing whatever title I see on the login screen. -->
 				<tr>
+					<td>4487452668036984689</td>
 					<td><code>2023.07.26.16.38</code></td>
 					<td><!-- Update 33.6: -->Echoes of Duviri</td>
-					<td>4487452668036984689</td>
+					<td></td>
 				</tr>
 				<tr>
+					<td>7706746084885057576</td>
 					<td><code>2023.06.22.13.21</code></td>
 					<td><!-- Update 33.5: -->The Seven Crimes of Kullervo</td>
-					<td>7706746084885057576</td>
+					<td></td>
 				</tr>
 				<tr>
+					<td>1040973097284770411</td>
 					<td><code>2023.06.21.07.11</code></td>
 					<td><!-- Update 33.5: -->The Seven Crimes of Kullervo</td>
-					<td>1040973097284770411</td>
+					<td></td>
 				</tr>
 				<tr>
+					<td>503269306359201928</td>
 					<td><code>2023.04.25.23.40</code></td>
 					<td><!-- Update 33: -->The Duviri Paradox</td>
-					<td>503269306359201928</td>
+					<td></td>
 				</tr>
 				<tr>
+					<td>2084947150827453084</td>
 					<td><code>2023.02.13.10.22</code></td>
 					<td><!-- Update 32.3: -->Citrine's Last Wish</td>
-					<td>2084947150827453084</td>
+					<td></td>
 				</tr>
 				<!-- 1591604753021484150 -->
 				<tr>
+					<td>2433324865707385268</td>
 					<td><code>2022.09.06.19.24</code></td>
 					<td><!-- Update 32: -->Veilbreaker</td>
-					<td>2433324865707385268</td>
+					<td></td>
 				</tr>
-				<!-- 7280281005656793707 -->
+				<tr>
+					<td>7280281005656793707</td>
+					<td><code>2022.07.25.16.59</code></td>
+					<td>Echoes of the Zariman<!-- : Update 31.6 --></td>
+					<td></td>
+				</tr>
 				<!-- 8725083537193522917 -->
 				<tr>
+					<td>4634493262513910961</td>
 					<td><code>2022.04.29.12.53</code></td>
 					<td><!-- Update 31.5: -->Angels of the Zariman</td>
-					<td>4634493262513910961</td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>1198361709927223847</td>
+					<td><code>2022.02.09.08.55</code></td>
+					<td><!-- Update 31: --> The New War</td>
+					<td>Defaults to low res fullscreen</td>
+				</tr>
+				<tr>
+					<td>563653176338996292</td>
+					<td><code>2021.12.15.00.15</code></td>
+					<td><!-- Update 31: --> The New War</td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>8243603931064121923</td>
+					<td><code>2021.09.08.19.27</code></td>
+					<td>Sisters of Parvos<!-- : Update 30.5 --></td>
+					<td></td>
+				</tr>
+				<!-- 4538343592657854566 -->
+				<!-- 1659398175797234554 -->
+				<!-- 896988770099174429 -->
+				<tr>
+					<td>1516755954909330587</td>
+					<td><code>2021.05.25.09.41</code></td>
+					<td>Call of the Tempestarii<!-- : Update 30 --></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>1471695976410467945</td>
+					<td><code>2021.04.13.19.58</code></td>
+					<td>Call of the Tempestarii<!-- : Update 30 --></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>2360447165794825746</td>
+					<td><code>2021.03.19.10.30</code></td>
+					<td><!-- Update 29.6.0: -->Orphix Venom</td>
+					<td></td>
+				</tr>
+				<!-- 5695018007407901588 -->
+				<!-- 2281391866826940520 -->
+				<tr>
+					<td>4589169429267220816</td>
+					<td><code>2020.11.04.18.58</code></td>
+					<td>Heart of Deimos<!-- : Update 29 --></td>
+					<td>Defaults to low res fullscreen</td>
+				</tr>
+				<tr>
+					<td>1198696432420157033</td>
+					<td><code>2020.08.25.18.35</code></td>
+					<td>Heart of Deimos<!-- : Update 29 --></td>
+					<td>Defaults to low res fullscreen</td>
+				</tr>
+				<tr>
+					<td>4862052161935324630</td>
+					<td><code>2020.08.12.19.57</code></td>
+					<td><!-- Update 28: -->The Deadlock Protocol</td>
+					<td>Defaults to low res fullscreen</td>
+				</tr>
+				<tr>
+					<td>8322392782655512672</td>
+					<td><code>2020.06.12.16.46</code></td>
+					<td><!-- Update 28: -->The Deadlock Protocol</td>
+					<td>Defaults to low res fullscreen</td>
+				</tr>
+				<tr>
+					<td>940429389442729133</td>
+					<td><code>2020.03.24.20.24</code></td>
+					<td>Empyrean<!-- : Update 27 --></td>
+					<td></td>
+				</tr>
+				<!-- 3078917755629993445 -->
+				<tr>
+					<td>5383208407459190147</td>
+					<td><code>2019.12.13.15.04</code></td>
+					<td>Empyrean<!-- : Update 27 --></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>1746499861411165604</td>
+					<td><code>2019.12.13.00.31</code></td>
+					<td>Rising Tide<!-- : Update 26.1 --></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>5191423722994130705</td>
+					<td><code>2019.11.22.21.24</code></td>
+					<td>Rising Tide<!-- : Update 26.1 --></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>4211752956119480973</td>
+					<td><code>2019.11.05.12.52</code></td>
+					<td><!-- Update 26: -->The Old Blood</td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>5213493012981659231</td>
+					<td><code>2019.10.31.22.42</code></td>
+					<td><!-- Update 26: -->The Old Blood</td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>6484357939617346090</td>
+					<td><code>2019.09.09.12.43</code></td>
+					<td>Saint of Altra<!-- : Update 25.7.0 --></td>
+					<td></td>
+				</tr>
+				<!-- 6955217559566015186 -->
+				<tr>
+					<td>8839989774604564585</td>
+					<td><code>2019.08.29.20.01</code></td>
+					<td>Saint of Altra<!-- : Update 25.7.0 --></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>3318532863308652389</td>
+					<td><code>2019.05.22.23.12</code></td>
+					<td>The Jovian Concord<!-- : Update 25 --></td>
+					<td></td>
 				</tr>
 			</tbody>
 		</table>
-		<p>To download an old version, you can use <a href="https://github.com/SteamRE/DepotDownloader/releases">DepotDownloader</a> like so: <code>DepotDownloader.exe -app 230410 -depot 230411 -manifest &lt;manifest id&gt; -username &lt;your steam username&gt;</code></p>
+		<p>To download an old version, you can use <a href="https://github.com/SteamRE/DepotDownloader/releases">DepotDownloader</a> like so: <code>DepotDownloader -app 230410 -depot 230411 -manifest &lt;manifest id&gt; -username &lt;your steam username&gt;</code></p>
 		<h3>2013 Warframe</h3>
 		<p>There is a DLL + fork of SpaceNinjaServer that can be used to revisit 2013 Warframe. More information about this can be found <a href="https://discord.gg/PNNZ3asUuY">on our Discord</a>, specifically <a href="https://discord.com/channels/1108159019635462206/1280628166825873510/1280628233355923497">in this thread</a>.</p>
 	</div>
