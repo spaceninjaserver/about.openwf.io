25856e7077642267450f7d26711d73e9911a1dbb
Sainan <63328889+Sainan@users.noreply.github.com>
2025-05-04 16:23:15 +0200

    Update old versions for bootstrapper 0.10.12

--- old-versions.php
+++ old-versions.php
@@ -14,9 +14,8 @@
 			<thead>
 				<tr>
 					<th>Steam Manifest ID</th>
-					<th>Version</th>
+					<th colspan="2">Version</th>
 					<th>Title</th>
-					<th>Notes</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -24,126 +23,135 @@
 				<tr>
 					<td>3864063200081954630</td>
 					<td><code>2024.12.11.14.26</code></td>
-					<td>Early build of Warframe: 1999</td>
-					<td></td>
+					<td>&lt; 38.0.0</td>
+					<td>1999</td>
 				</tr>
 				<tr>
 					<td>513490184226321110</td>
 					<td><code>2024.10.11.10.06</code></td>
-					<td>Koumei & the Five Fates: Hotfix 37.0.6</td>
-					<td></td>
+					<td>= 37.0.6</td>
+					<td>Koumei & the Five Fates</td>
 				</tr>
 				<tr>
 					<td>2655930801276651913</td>
 					<td><code>2024.09.30.16.56</code></td>
-					<td>Early build of Koumei & the Five Fates</td>
-					<td></td>
+					<td>&lt; 37.0.0</td>
+					<td>Koumei & the Five Fates</td>
 				</tr>
 				<tr>
 					<td>8111233839173446959</td>
 					<td><code>2024.08.21.20.02</code></td>
-					<td>The Lotus Eaters: Hotfix 36.1.2</td>
-					<td></td>
+					<td>= 36.1.2</td>
+					<td>The Lotus Eaters</td>
 				</tr>
 				<tr>
 					<td>7104716125379362906</td>
 					<td><code>2024.07.17.17.07</code></td>
-					<td>Jade Shadows: Hotfix 36.0.7</td>
-					<td></td>
+					<td>= 36.0.7</td>
+					<td>Jade Shadows</td>
 				</tr>
 				<tr>
 					<td>8456987056335242580</td>
 					<td><code>2024.06.25.16.44</code></td>
-					<td>Jade Shadows: Hotfix 36.0.4</td>
-					<td></td>
+					<td>= 36.0.4</td>
+					<td>Jade Shadows</td>
 				</tr>
 				<tr>
 					<td>8444137865636064549</td>
 					<td><code>2024.06.12.18.42</code></td>
-					<td>Early build of Jade Shadows</td>
-					<td></td>
+					<td>&lt; 36.0.0</td>
+					<td>Jade Shadows</td>
 				</tr>
 				<tr>
 					<td>4262681179882731333</td>
 					<td><code>2024.03.24.20.00</code></td>
-					<td>Update 35.5: Dante Unbound</td>
-					<td></td>
+					<td>= 35.5.0</td>
+					<td>Dante Unbound</td>
 				</tr>
 				<tr>
 					<td>3417254152679980994</td>
 					<td><code>2024.02.16.17.13</code></td>
-					<td>Whispers in the Walls: Update 35.1.0</td>
-					<td></td>
+					<td>= 35.1.0</td>
+					<td>Whispers in the Walls</td>
 				</tr>
 				<!-- Also verified that 34.0.8 works, but sadly there's no manifest id for any of U34.-->
-				<!-- Beyond this point, there is no information to help correlate the build labels, so I'm just writing whatever title I see on the login screen. -->
 				<tr>
 					<td>4487452668036984689</td>
 					<td><code>2023.07.26.16.38</code></td>
-					<td><!-- Update 33.6: -->Echoes of Duviri</td>
-					<td></td>
+					<td>≈ 33.6.0</td>
+					<td>Echoes of Duviri</td>
 				</tr>
 				<tr>
 					<td>7706746084885057576</td>
 					<td><code>2023.06.22.13.21</code></td>
-					<td><!-- Update 33.5: -->The Seven Crimes of Kullervo</td>
-					<td></td>
+					<td>≈ 33.5.2</td>
+					<td>The Seven Crimes of Kullervo</td>
 				</tr>
 				<tr>
 					<td>1040973097284770411</td>
 					<td><code>2023.06.21.07.11</code></td>
-					<td><!-- Update 33.5: -->The Seven Crimes of Kullervo</td>
-					<td></td>
+					<td>≈ 33.5.0</td>
+					<td>The Seven Crimes of Kullervo</td>
 				</tr>
 				<tr>
 					<td>503269306359201928</td>
 					<td><code>2023.04.25.23.40</code></td>
-					<td><!-- Update 33: -->The Duviri Paradox</td>
-					<td></td>
+					<td>≈ 33.0.0</td>
+					<td>The Duviri Paradox</td>
 				</tr>
 				<tr>
 					<td>2084947150827453084</td>
 					<td><code>2023.02.13.10.22</code></td>
-					<td><!-- Update 32.3: -->Citrine's Last Wish</td>
-					<td></td>
+					<td>≈ 32.3.0</td>
+					<td>Citrine's Last Wish</td>
 				</tr>
-				<!-- 1591604753021484150 -->
+				<tr>
+					<td>1591604753021484150</td>
+					<td><code>2022.11.30.08.13</code></td>
+					<td>≈ 32.2.0</td>
+					<td>Lua's Prey</td>
+				</tr>
 				<tr>
 					<td>2433324865707385268</td>
 					<td><code>2022.09.06.19.24</code></td>
-					<td><!-- Update 32: -->Veilbreaker</td>
-					<td></td>
+					<td>≈ 32.0.0</td>
+					<td>Veilbreaker</td>
 				</tr>
 				<tr>
 					<td>7280281005656793707</td>
 					<td><code>2022.07.25.16.59</code></td>
-					<td>Echoes of the Zariman<!-- : Update 31.6 --></td>
-					<td></td>
+					<td>≈ 31.7.1</td>
+					<td>Echoes of the Zariman</td>
 				</tr>
-				<!-- 8725083537193522917 -->
+				<tr>
+					<td>8725083537193522917</td>
+					<td><code>2022.06.09.08.45</code></td>
+					<td>≈ 31.6.0</td>
+					<td>Echoes of the Zariman</td>
+				</tr>
 				<tr>
 					<td>4634493262513910961</td>
 					<td><code>2022.04.29.12.53</code></td>
-					<td><!-- Update 31.5: -->Angels of the Zariman</td>
-					<td></td>
+					<td>≈ 31.5.0</td>
+					<td>Angels of the Zariman</td>
 				</tr>
 				<tr>
 					<td>1198361709927223847</td>
 					<td><code>2022.02.09.08.55</code></td>
-					<td><!-- Update 31: --> The New War</td>
-					<td>Defaults to low res fullscreen</td>
+					<td>≈ 31.1.0</td>
+					<td>The New War</td>
 				</tr>
 				<tr>
 					<td>563653176338996292</td>
 					<td><code>2021.12.15.00.15</code></td>
-					<td><!-- Update 31: --> The New War</td>
-					<td></td>
+					<td>≈ 31.0.0</td>
+					<td>The New War</td>
 				</tr>
 				<tr>
 					<td>8243603931064121923</td>
 					<td><code>2021.09.08.19.27</code></td>
-					<td>Sisters of Parvos<!-- : Update 30.5 --></td>
-					<td></td>
+					<td>≈ 30.7.0</td>
+					<td>Sisters of Parvos</td>
 				</tr>
 				<!-- 4538343592657854566 -->
 				<!-- 1659398175797234554 -->
@@ -151,103 +159,242 @@
 				<tr>
 					<td>1516755954909330587</td>
 					<td><code>2021.05.25.09.41</code></td>
-					<td>Call of the Tempestarii<!-- : Update 30 --></td>
-					<td></td>
+					<td>≈ 30.3.0</td>
+					<td>Call of the Tempestarii</td>
 				</tr>
 				<tr>
 					<td>1471695976410467945</td>
 					<td><code>2021.04.13.19.58</code></td>
-					<td>Call of the Tempestarii<!-- : Update 30 --></td>
-					<td></td>
+					<td>≈ 30.0.0</td>
+					<td>Call of the Tempestarii</td>
 				</tr>
 				<tr>
 					<td>2360447165794825746</td>
 					<td><code>2021.03.19.10.30</code></td>
-					<td><!-- Update 29.6.0: -->Orphix Venom</td>
-					<td></td>
+					<td>≈ 29.10.0</td>
+					<td>Orphix Venom</td>
 				</tr>
-				<!-- 5695018007407901588 -->
-				<!-- 2281391866826940520 -->
+				<tr>
+					<td>5695018007407901588</td>
+					<td><code>2021.01.25.08.49</code></td>
+					<td>≈ 29.6.8</td>
+					<td>Orphix Venom</td>
+				</tr>
+				<tr>
+					<td>2281391866826940520</td>
+					<td><code>2020.11.19.17.52</code></td>
+					<td>≈ 29.5.0</td>
+					<td>Echoes of Deimos<!-- Login screen says "Heart of Deimos" --></td>
+				</tr>
 				<tr>
 					<td>4589169429267220816</td>
 					<td><code>2020.11.04.18.58</code></td>
-					<td>Heart of Deimos<!-- : Update 29 --></td>
-					<td>Defaults to low res fullscreen</td>
+					<td>≈ 29.3.1</td>
+					<td>Heart of Deimos</td>
 				</tr>
 				<tr>
 					<td>1198696432420157033</td>
 					<td><code>2020.08.25.18.35</code></td>
-					<td>Heart of Deimos<!-- : Update 29 --></td>
-					<td>Defaults to low res fullscreen</td>
+					<td>≈ 29.0.0</td>
+					<td>Heart of Deimos</td>
 				</tr>
 				<tr>
 					<td>4862052161935324630</td>
 					<td><code>2020.08.12.19.57</code></td>
-					<td><!-- Update 28: -->The Deadlock Protocol</td>
-					<td>Defaults to low res fullscreen</td>
+					<td>≈ 28.3.0</td>
+					<td>Derelict Shift<!-- Login screen says "The Deadlock Protocol" --></td>
 				</tr>
 				<tr>
 					<td>8322392782655512672</td>
 					<td><code>2020.06.12.16.46</code></td>
-					<td><!-- Update 28: -->The Deadlock Protocol</td>
-					<td>Defaults to low res fullscreen</td>
+					<td>≈ 28.0.0</td>
+					<td>The Deadlock Protocol</td>
 				</tr>
 				<tr>
 					<td>940429389442729133</td>
 					<td><code>2020.03.24.20.24</code></td>
+					<td>≈ 27.3.0</td>
 					<td>Empyrean<!-- : Update 27 --></td>
-					<td></td>
 				</tr>
 				<!-- 3078917755629993445 -->
 				<tr>
 					<td>5383208407459190147</td>
 					<td><code>2019.12.13.15.04</code></td>
-					<td>Empyrean<!-- : Update 27 --></td>
-					<td></td>
+					<td>≈ 27.0.0</td>
+					<td>Empyrean</td>
 				</tr>
 				<tr>
 					<td>1746499861411165604</td>
 					<td><code>2019.12.13.00.31</code></td>
-					<td>Rising Tide<!-- : Update 26.1 --></td>
-					<td></td>
+					<td>≈ 26.1.3</td>
+					<td>Rising Tide</td>
 				</tr>
 				<tr>
 					<td>5191423722994130705</td>
 					<td><code>2019.11.22.21.24</code></td>
-					<td>Rising Tide<!-- : Update 26.1 --></td>
-					<td></td>
+					<td>≈ 26.1.0</td>
+					<td>Rising Tide</td>
 				</tr>
 				<tr>
 					<td>4211752956119480973</td>
 					<td><code>2019.11.05.12.52</code></td>
-					<td><!-- Update 26: -->The Old Blood</td>
-					<td></td>
+					<td>≈ 26.0.4</td>
+					<td>The Old Blood</td>
 				</tr>
 				<tr>
 					<td>5213493012981659231</td>
 					<td><code>2019.10.31.22.42</code></td>
-					<td><!-- Update 26: -->The Old Blood</td>
-					<td></td>
+					<td>≈ 26.0.0</td>
+					<td>The Old Blood</td>
 				</tr>
 				<tr>
 					<td>6484357939617346090</td>
 					<td><code>2019.09.09.12.43</code></td>
-					<td>Saint of Altra<!-- : Update 25.7.0 --></td>
-					<td></td>
+					<td>≈ 25.7.5</td>
+					<td>Saint of Altra</td>
 				</tr>
-				<!-- 6955217559566015186 -->
+				<tr>
+					<td>6955217559566015186 </td>
+					<td><code>2019.08.29.21.58</code></td>
+					<td>≈ 25.7.0</td>
+					<td>Saint of Altra</td>
+				</tr>
 				<tr>
 					<td>8839989774604564585</td>
 					<td><code>2019.08.29.20.01</code></td>
-					<td>Saint of Altra<!-- : Update 25.7.0 --></td>
-					<td></td>
+					<td>≈ 25.7.0</td>
+					<td>Saint of Altra</td>
 				</tr>
 				<tr>
 					<td>3318532863308652389</td>
 					<td><code>2019.05.22.23.12</code></td>
-					<td>The Jovian Concord<!-- : Update 25 --></td>
-					<td></td>
+					<td>≈ 25.0.0</td>
+					<td>The Jovian Concord</td>
 				</tr>
+				<tr>
+					<td>192898029640470386</td>
+					<td><code>2019.04.04.21.31</code></td>
+					<td>≈ 24.6.0</td>
+					<td>Buried Debts</td>
+				</tr>
+				<!-- 4942488930993990244 -->
+				<!-- 4404971551504983924 -->
+				<tr>
+					<td>1904743723246597237</td>
+					<td><code>2019.03.07.20.21</code></td>
+					<td>≈ 24.4.0</td>
+					<td>Fortuna</td>
+				</tr>
+				<!-- 174676543101364914 is 2018.11.29.15.55, Fortuna, but has a broken font. -->
+				<tr>
+					<td>8040761247114336449</td>
+					<td><code>2018.11.29.15.55</code></td>
+					<td>≈ 24.1.2</td>
+					<td>Fortuna</td>
+				</tr>
+				<!-- 3725569727599851360 -->
+				<!-- 8461211368258506784 -->
+				<tr>
+					<td>6046541448142309692</td>
+					<td><code>2018.11.08.14.45</code></td>
+					<td>≈ 24.0.0</td>
+					<td>Fortuna</td>
+				</tr>
+				<!-- 8049567794324118250 -->
+				<!-- 1234607834806579897 -->
+				<!-- 677450530474051174 -->
+				<tr>
+					<td>8058901711639962245</td>
+					<td><code>2018.06.14.23.21</code></td>
+					<td>≈ 23.0.0</td>
+					<td>The Sacrifice<!-- Login screen says "Beasts of the Sanctuary" --></td>
+				</tr>
+				<tr>
+					<td>566447983471491143</td>
+					<td><code>2018.05.17.16.28</code></td>
+					<td>≈ 22.20.0</td>
+					<td>Beasts of the Sanctuary</td>
+				</tr>
+				<tr>
+					<td>8448828347811631025</td>
+					<td><code>2018.04.20.02.04</code></td>
+					<td>≈ 22.18.0</td>
+					<td>Beasts of the Sanctuary</td>
+				</tr>
+				<tr>
+					<td>5922375304465722812</td>
+					<td><code>2018.03.15.19.39</code></td>
+					<td>≈ 22.16.0</td>
+					<td>Plains of Eidolon</td>
+				</tr>
+				<tr>
+					<td>6640882772717519747</td>
+					<td><code>2018.03.07.14.18</code></td>
+					<td>≈ 22.15.0</td>
+					<td>Plains of Eidolon</td>
+				</tr>
+				<tr>
+					<td>8004325165498360760</td>
+					<td><code>2018.02.22.14.34</code></td>
+					<td>≈ 22.13.4</td>
+					<td>Plains of Eidolon</td>
+				</tr>
+				<!-- 8621631773919294851 -->
+				<!-- 4946799727072561536 -->
+				<!-- 5301169201249491747 -->
+				<!-- 5914709226935570547 -->
+				<!-- 5869273198520287595 -->
+				<tr>
+					<td>4661126446448856114</td>
+					<td><code>2017.10.12.17.04</code></td>
+					<td>≈ 22.0.0</td>
+					<td>Plains of Eidolon</td>
+					<!-- Need to go through tutorial v2 -->
+				</tr>
+				<tr>
+					<td>1835327182756411758</td>
+					<td><code>2017.07.26.13.59</code></td>
+					<td>≈ 21.2.0</td>
+					<td>Chains of Harrow</td>
+					<!-- Need to go through tutorial v2, has some horrible stutters -->
+				</tr>
+				<!-- 2970877824413815022 might be ~21.0.2 -->
+				<tr>
+					<td>9187782625344917768</td>
+					<td><code>2017.06.29.02.13</code></td>
+					<td>≈ 21.0.0</td>
+					<td>Chains of Harrow</td>
+					<!-- Need to go through tutorial v2 but can skip its cutscenes -->
+				</tr>
+				<!-- 3099723078202393524 should be ~20.4.4 -->
+				<tr>
+					<td>4574874014936523212</td>
+					<td><code>2017.03.26.04.21</code></td>
+					<td>≈ 20.0.3</td>
+					<td>Octavia's Anthem</td>
+					<!-- Need to go through tutorial v2 but can skip its cutscenes -->
+				</tr>
+				<!-- 8705016425420648723 is 2017.03.24.17.25, ~20.0.0, seems to have a broken cache as the loading scaleform doesn't show properly and it doesn't proceed to the login screen -->
+				<tr>
+					<td>6263571497645662720</td>
+					<td><code>2017.03.06.15.49</code></td>
+					<td>≈ 19.13.0</td>
+					<td>The Glast Gambit</td>
+					<!-- Need to go through tutorial v2 but can skip its cutscenes -->
+				</tr>
+				<!-- 2663512682334598162 -->
+				<!-- 4864838558963648894 -->
+				<!-- 4666421798085404101 -->
+				<!-- 6118281920152298389 -->
+				<!-- 8998041413872250076 -->
+				<!-- 6784000424578492584 is 2016.11.11.17.46, ~19.0.0, fails to start due to an assertion failure -->
+				<!-- <tr>
+					<td>7585839495921050946</td>
+					<td><code>2016.09.30.12.04</code></td>
+					<td>≈ 18.22.1</td>
+					<td>The Silver Grove</td>
+					Needs some work still, the HTTP request stuff here is a bit different.
+				</tr> -->
 			</tbody>
 		</table>
 		<p>To download an old version, you can use <a href="https://github.com/SteamRE/DepotDownloader/releases">DepotDownloader</a> like so: <code>DepotDownloader -app 230410 -depot 230411 -manifest &lt;manifest id&gt; -username &lt;your steam username&gt;</code></p>
