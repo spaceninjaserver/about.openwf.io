e5ecc596dc7aaf26b44464447ab4a1e74c7fc6a1
Sainan <63328889+Sainan@users.noreply.github.com>
2025-05-05 14:04:58 +0200

    Note versions which load into tutorial

--- old-versions.php
+++ old-versions.php
@@ -16,6 +16,7 @@
 					<th>Steam Manifest ID</th>
 					<th colspan="2">Version</th>
 					<th>Title</th>
+					<th>Notes</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -25,54 +26,63 @@
 					<td><code>2024.12.11.14.26</code></td>
 					<td>&lt; 38.0.0</td>
 					<td>1999</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>513490184226321110</td>
 					<td><code>2024.10.11.10.06</code></td>
 					<td>= 37.0.6</td>
 					<td>Koumei & the Five Fates</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>2655930801276651913</td>
 					<td><code>2024.09.30.16.56</code></td>
 					<td>&lt; 37.0.0</td>
 					<td>Koumei & the Five Fates</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>8111233839173446959</td>
 					<td><code>2024.08.21.20.02</code></td>
 					<td>= 36.1.2</td>
 					<td>The Lotus Eaters</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>7104716125379362906</td>
 					<td><code>2024.07.17.17.07</code></td>
 					<td>= 36.0.7</td>
 					<td>Jade Shadows</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>8456987056335242580</td>
 					<td><code>2024.06.25.16.44</code></td>
 					<td>= 36.0.4</td>
 					<td>Jade Shadows</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>8444137865636064549</td>
 					<td><code>2024.06.12.18.42</code></td>
 					<td>&lt; 36.0.0</td>
 					<td>Jade Shadows</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>4262681179882731333</td>
 					<td><code>2024.03.24.20.00</code></td>
 					<td>= 35.5.0</td>
 					<td>Dante Unbound</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>3417254152679980994</td>
 					<td><code>2024.02.16.17.13</code></td>
 					<td>= 35.1.0</td>
 					<td>Whispers in the Walls</td>
+					<td></td>
 				</tr>
 				<!-- Also verified that 34.0.8 works, but sadly there's no manifest id for any of U34.-->
 				<tr>
@@ -80,78 +90,91 @@
 					<td><code>2023.07.26.16.38</code></td>
 					<td>≈ 33.6.0</td>
 					<td>Echoes of Duviri</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>7706746084885057576</td>
 					<td><code>2023.06.22.13.21</code></td>
 					<td>≈ 33.5.2</td>
 					<td>The Seven Crimes of Kullervo</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>1040973097284770411</td>
 					<td><code>2023.06.21.07.11</code></td>
 					<td>≈ 33.5.0</td>
 					<td>The Seven Crimes of Kullervo</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>503269306359201928</td>
 					<td><code>2023.04.25.23.40</code></td>
 					<td>≈ 33.0.0</td>
 					<td>The Duviri Paradox</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>2084947150827453084</td>
 					<td><code>2023.02.13.10.22</code></td>
 					<td>≈ 32.3.0</td>
 					<td>Citrine's Last Wish</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>1591604753021484150</td>
 					<td><code>2022.11.30.08.13</code></td>
 					<td>≈ 32.2.0</td>
 					<td>Lua's Prey</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>2433324865707385268</td>
 					<td><code>2022.09.06.19.24</code></td>
 					<td>≈ 32.0.0</td>
 					<td>Veilbreaker</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>7280281005656793707</td>
 					<td><code>2022.07.25.16.59</code></td>
 					<td>≈ 31.7.1</td>
 					<td>Echoes of the Zariman</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>8725083537193522917</td>
 					<td><code>2022.06.09.08.45</code></td>
 					<td>≈ 31.6.0</td>
 					<td>Echoes of the Zariman</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>4634493262513910961</td>
 					<td><code>2022.04.29.12.53</code></td>
 					<td>≈ 31.5.0</td>
 					<td>Angels of the Zariman</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>1198361709927223847</td>
 					<td><code>2022.02.09.08.55</code></td>
 					<td>≈ 31.1.0</td>
 					<td>The New War</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>563653176338996292</td>
 					<td><code>2021.12.15.00.15</code></td>
 					<td>≈ 31.0.0</td>
 					<td>The New War</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>8243603931064121923</td>
 					<td><code>2021.09.08.19.27</code></td>
 					<td>≈ 30.7.0</td>
 					<td>Sisters of Parvos</td>
+					<td></td>
 				</tr>
 				<!-- 4538343592657854566 -->
 				<!-- 1659398175797234554 -->
@@ -161,60 +184,70 @@
 					<td><code>2021.05.25.09.41</code></td>
 					<td>≈ 30.3.0</td>
 					<td>Call of the Tempestarii</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>1471695976410467945</td>
 					<td><code>2021.04.13.19.58</code></td>
 					<td>≈ 30.0.0</td>
 					<td>Call of the Tempestarii</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>2360447165794825746</td>
 					<td><code>2021.03.19.10.30</code></td>
 					<td>≈ 29.10.0</td>
 					<td>Orphix Venom</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>5695018007407901588</td>
 					<td><code>2021.01.25.08.49</code></td>
 					<td>≈ 29.6.8</td>
 					<td>Orphix Venom</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>2281391866826940520</td>
 					<td><code>2020.11.19.17.52</code></td>
 					<td>≈ 29.5.0</td>
 					<td>Echoes of Deimos<!-- Login screen says "Heart of Deimos" --></td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>4589169429267220816</td>
 					<td><code>2020.11.04.18.58</code></td>
 					<td>≈ 29.3.1</td>
 					<td>Heart of Deimos</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>1198696432420157033</td>
 					<td><code>2020.08.25.18.35</code></td>
 					<td>≈ 29.0.0</td>
 					<td>Heart of Deimos</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>4862052161935324630</td>
 					<td><code>2020.08.12.19.57</code></td>
 					<td>≈ 28.3.0</td>
 					<td>Derelict Shift<!-- Login screen says "The Deadlock Protocol" --></td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>8322392782655512672</td>
 					<td><code>2020.06.12.16.46</code></td>
 					<td>≈ 28.0.0</td>
 					<td>The Deadlock Protocol</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>940429389442729133</td>
 					<td><code>2020.03.24.20.24</code></td>
 					<td>≈ 27.3.0</td>
 					<td>Empyrean<!-- : Update 27 --></td>
+					<td></td>
 				</tr>
 				<!-- 3078917755629993445 -->
 				<tr>
@@ -222,60 +255,70 @@
 					<td><code>2019.12.13.15.04</code></td>
 					<td>≈ 27.0.0</td>
 					<td>Empyrean</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>1746499861411165604</td>
 					<td><code>2019.12.13.00.31</code></td>
 					<td>≈ 26.1.3</td>
 					<td>Rising Tide</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>5191423722994130705</td>
 					<td><code>2019.11.22.21.24</code></td>
 					<td>≈ 26.1.0</td>
 					<td>Rising Tide</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>4211752956119480973</td>
 					<td><code>2019.11.05.12.52</code></td>
 					<td>≈ 26.0.4</td>
 					<td>The Old Blood</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>5213493012981659231</td>
 					<td><code>2019.10.31.22.42</code></td>
 					<td>≈ 26.0.0</td>
 					<td>The Old Blood</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>6484357939617346090</td>
 					<td><code>2019.09.09.12.43</code></td>
 					<td>≈ 25.7.5</td>
 					<td>Saint of Altra</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>6955217559566015186 </td>
 					<td><code>2019.08.29.21.58</code></td>
 					<td>≈ 25.7.0</td>
 					<td>Saint of Altra</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>8839989774604564585</td>
 					<td><code>2019.08.29.20.01</code></td>
 					<td>≈ 25.7.0</td>
 					<td>Saint of Altra</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>3318532863308652389</td>
 					<td><code>2019.05.22.23.12</code></td>
 					<td>≈ 25.0.0</td>
 					<td>The Jovian Concord</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>192898029640470386</td>
 					<td><code>2019.04.04.21.31</code></td>
 					<td>≈ 24.6.0</td>
 					<td>Buried Debts</td>
+					<td></td>
 				</tr>
 				<!-- 4942488930993990244 -->
 				<!-- 4404971551504983924 -->
@@ -284,6 +327,7 @@
 					<td><code>2019.03.07.20.21</code></td>
 					<td>≈ 24.4.0</td>
 					<td>Fortuna</td>
+					<td></td>
 				</tr>
 				<!-- 174676543101364914 is 2018.11.29.15.55, Fortuna, but has a broken font. -->
 				<tr>
@@ -291,6 +335,7 @@
 					<td><code>2018.11.29.15.55</code></td>
 					<td>≈ 24.1.2</td>
 					<td>Fortuna</td>
+					<td></td>
 				</tr>
 				<!-- 3725569727599851360 -->
 				<!-- 8461211368258506784 -->
@@ -299,6 +344,7 @@
 					<td><code>2018.11.08.14.45</code></td>
 					<td>≈ 24.0.0</td>
 					<td>Fortuna</td>
+					<td></td>
 				</tr>
 				<!-- 8049567794324118250 -->
 				<!-- 1234607834806579897 -->
@@ -308,36 +354,42 @@
 					<td><code>2018.06.14.23.21</code></td>
 					<td>≈ 23.0.0</td>
 					<td>The Sacrifice<!-- Login screen says "Beasts of the Sanctuary" --></td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>566447983471491143</td>
 					<td><code>2018.05.17.16.28</code></td>
 					<td>≈ 22.20.0</td>
 					<td>Beasts of the Sanctuary</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>8448828347811631025</td>
 					<td><code>2018.04.20.02.04</code></td>
 					<td>≈ 22.18.0</td>
 					<td>Beasts of the Sanctuary</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>5922375304465722812</td>
 					<td><code>2018.03.15.19.39</code></td>
 					<td>≈ 22.16.0</td>
 					<td>Plains of Eidolon</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>6640882772717519747</td>
 					<td><code>2018.03.07.14.18</code></td>
 					<td>≈ 22.15.0</td>
 					<td>Plains of Eidolon</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>8004325165498360760</td>
 					<td><code>2018.02.22.14.34</code></td>
 					<td>≈ 22.13.4</td>
 					<td>Plains of Eidolon</td>
+					<td></td>
 				</tr>
 				<!-- 8621631773919294851 -->
 				<!-- 4946799727072561536 -->
@@ -349,14 +401,14 @@
 					<td><code>2017.10.12.17.04</code></td>
 					<td>≈ 22.0.0</td>
 					<td>Plains of Eidolon</td>
-					<!-- Need to go through tutorial v2 -->
+					<td>Loads into tutorial</td>
 				</tr>
 				<tr>
 					<td>1835327182756411758</td>
 					<td><code>2017.07.26.13.59</code></td>
 					<td>≈ 21.2.0</td>
 					<td>Chains of Harrow</td>
-					<!-- Need to go through tutorial v2, has some horrible stutters -->
+					<td>Loads into tutorial</td>
 				</tr>
 				<!-- 2970877824413815022 might be ~21.0.2 -->
 				<tr>
@@ -364,7 +416,7 @@
 					<td><code>2017.06.29.02.13</code></td>
 					<td>≈ 21.0.0</td>
 					<td>Chains of Harrow</td>
-					<!-- Need to go through tutorial v2 but can skip its cutscenes -->
+					<td>Loads into tutorial</td>
 				</tr>
 				<!-- 3099723078202393524 should be ~20.4.4 -->
 				<tr>
@@ -372,7 +424,7 @@
 					<td><code>2017.03.26.04.21</code></td>
 					<td>≈ 20.0.3</td>
 					<td>Octavia's Anthem</td>
-					<!-- Need to go through tutorial v2 but can skip its cutscenes -->
+					<td>Loads into tutorial</td>
 				</tr>
 				<!-- 8705016425420648723 is 2017.03.24.17.25, ~20.0.0, seems to have a broken cache as the loading scaleform doesn't show properly and it doesn't proceed to the login screen -->
 				<tr>
@@ -380,7 +432,7 @@
 					<td><code>2017.03.06.15.49</code></td>
 					<td>≈ 19.13.0</td>
 					<td>The Glast Gambit</td>
-					<!-- Need to go through tutorial v2 but can skip its cutscenes -->
+					<td>Loads into tutorial</td>
 				</tr>
 				<!-- 2663512682334598162 -->
 				<!-- 4864838558963648894 -->
