e5ff927736625611b7863fb089d676abc850f11d
Sainan <63328889+Sainan@users.noreply.github.com>
2025-11-29 17:23:49 +0100

    Note that SpaceNinjaServer is not open-source

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 		<p>OpenWF is a community project to play Warframe offline, specifically for preservation and customization.</p>
 		<h3>Software</h3>
 		<ul>
-			<li><b>SpaceNinjaServer</b> is a customizable, <a href="https://onlyg.it/OpenWF/SpaceNinjaServer" target="_blank">open-source</a> reimplementation of the game's web services.</li>
+			<li><b>SpaceNinjaServer</b> is a customizable, <abbr title="You may be familiar with the term open-source, but the official definition includes commercial use which is explicitly prohibited by SpaceNinjaServer's license.">source-available</abbr> reimplementation of the game's web services. <a href="https://onlyg.it/OpenWF/SpaceNinjaServer" target="_blank">Browse code.</a></li>
 			<li>The <b>Bootstrapper</b> patches the game client to connect to custom servers, but for newer versions, it also has some QoL features and cheats as well as a scripting API, label replacements and metadata patches, allowing for extensive customization of the experience. <a href="https://onlyg.it/OpenWF/Mods" target="_blank">Browse mods.</a></li>
 		</ul>
 		<a href="guide<?=$ext;?>" class="btn btn-primary">Get Started</a>
