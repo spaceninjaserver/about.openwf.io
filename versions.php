64783f4b316b052d2f446010641371238907f5eb
Sainan <63328889+Sainan@users.noreply.github.com>
2026-05-01 03:28:14 +0200

    Versions: Add 16.1.4.1

--- versions.php
+++ versions.php
@@ -1012,6 +1012,14 @@
 						<td>Steam&nbsp;manifest</td>
 						<td><a href="#232461576962714068">Download</a></td>
 					</tr>
+					<tr id="16.1.4.1" data-langs="zh" data-drivers="dx11,dx10,dx9" data-magnet="magnet:?xt=urn:btih:259e1363b839420d0cddcc5fd12fa7d6e451a6cb&dn=16.1.4.1%20%28zh%29&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&ws=https://archive.org/download/Warframe-16.1.4.1/">
+						<td><code>2015.03.28.18.05</code></td>
+						<td>≈&nbsp;16.1.4.1</td>
+						<td>Tubemen&nbsp;of&nbsp;Regor</td>
+						<td><span class="table-only-note">Chinese only</span><!-- Rename dwmapi.dll to wtsapi32.dll or version.dll. --></td>
+						<td>User&nbsp;installation</td>
+						<td><a href="#16.1.4.1">Download</a></td>
+					</tr>
 					<tr id="16.1.3" data-langs="zh" data-drivers="dx11,dx10,dx9" data-magnet="magnet:?xt=urn:btih:a8d21e8db8ed32c7b995039e4bc450039fe0eeed&dn=16.1.3%20%28zh%29&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&ws=https://archive.org/download/Warframe-16.1.3/">
 						<td><code>2015.03.26.16.15</code></td>
 						<td>≈&nbsp;16.1.3</td>
